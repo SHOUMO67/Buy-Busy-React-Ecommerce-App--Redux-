e1cef0c9918f7e4d8d12b99ae143eac83395d069
SHOUMO67 <115798128+SHOUMO67@users.noreply.github.com>
2024-07-21 14:05:01 +0530

    firebaseinit.js

--- src/firebaseinit.js
+++ src/firebaseinit.js
@@ -7,14 +7,7 @@ import { getAuth } from "firebase/auth";
 
 
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDkzFYHT1q9ssEbPC-Plf__dctPFLN7Ndc",
-  authDomain: "my-project-4ad12.firebaseapp.com",
-  projectId: "my-project-4ad12",
-  storageBucket: "my-project-4ad12.appspot.com",
-  messagingSenderId: "637268187172",
-  appId: "1:637268187172:web:0ecd04ca1c3bed7575a0b1"
-};
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
